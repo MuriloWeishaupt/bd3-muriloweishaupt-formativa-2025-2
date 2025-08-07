baf51892d3dd939c17b43c4433b86d4c277b265b
MWeishaupt <113941985+MuriloWeishaupt@users.noreply.github.com>
2025-08-07 01:13:06 +0000

    BD3: criação de banco de dados, criação de coleção, inserção de dados one e many

---
+++ criando-bd.mongodb.js
@@ -0,0 +1,18 @@
+// Criação da constante que representa o nome do banco de dados
+const database = 'BD3-AULAS';
+
+// Criação da constante que representa o nome da coleção de arquivos
+const collection = 'LIVRARIA';
+
+// Define o banco de dados que será criado, caso não exista, será criado
+use(database);
+
+// Cria uma coleção de dados
+db.createCollection(collection);
+
+
+
+
+
+
+
